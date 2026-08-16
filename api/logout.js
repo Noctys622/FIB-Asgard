ec121b459dfc1096e10320059aed12915d412bc1
Noctys622 <thibautrouelle@gmail.com>
2026-08-16 12:20:58 +0200

    Ajoute déconnexion Discord

---
+++ api/logout.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+  res.setHeader('Set-Cookie',
+    'fib_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0'
+  );
+  res.status(200).json({ ok: true });
+};
